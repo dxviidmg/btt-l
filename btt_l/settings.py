b15b5ea38f5dcf97c031269130c8d7b70ee6f1f9
David <david@ixulabs.com>
2021-05-01 18:58:59 -0500

    fix

--- btt_l/settings.py
+++ btt_l/settings.py
@@ -143,5 +143,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
-#EMAIL_HOST_USER = 'youremail'
-#EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_HOST_USER = 'youremail'
+EMAIL_HOST_PASSWORD = 'yourpassword'
