c29b83756c9dd9e20c9a80f3e48a409e4a98572a
David <david@ixulabs.com>
2021-05-01 20:12:01 -0500

    fix

--- products/tests.py
+++ products/tests.py
@@ -8,12 +8,11 @@ data_product = {'sku': '1', 'name':'Producto X', 'price': 10}
 class BrandModelTest(TestCase):
     def test_create_brand(self):
         brand, brand_created = Brand.objects.get_or_create(**data_brand)
-
         self.assertEquals(brand.name, 'Marca X')
+
 
 class ProductModelTest(TestCase):
     def test_create_product(self):
         brand, brand_created = Brand.objects.get_or_create(**data_brand)
         product, product_created = Product.objects.get_or_create(**data_product, brand=brand)
-
         self.assertEquals(product.name, 'Producto X')
