5d6b1c6bd2b6ea18b884b319f13e4425315c237f
David <david@ixulabs.com>
2021-05-01 20:03:52 -0500

    tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+data_user = {'username':'ejemplo', 'first_name': 'ejemplo'}
+password_user = 'testuser'
+
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user, user_created = User.objects.get_or_create(**data_user)
+        user.set_password(password_user)
+
+        self.assertEquals(user.username, 'ejemplo')
+
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from products.models import Brand, Product
 
-# Create your tests here.
+data_brand = {'name':'Marca X'}
+data_product = {'sku': '1', 'name':'Producto X', 'price': 10}
+
+
+class BrandModelTest(TestCase):
+    def test_create_brand(self):
+        brand, brand_created = Brand.objects.get_or_create(**data_brand)
+
+        self.assertEquals(brand.name, 'Marca X')
+
+class ProductModelTest(TestCase):
+    def test_create_product(self):
+        brand, brand_created = Brand.objects.get_or_create(**data_brand)
+        product, product_created = Product.objects.get_or_create(**data_product, brand=brand)
+
+        self.assertEquals(product.name, 'Producto X')
